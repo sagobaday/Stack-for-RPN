1733bdcee1bf91374f8e344c87c05a804540e2da
santiago b <santiagobadaya@gmail.com>
2023-05-29 19:36:20 +0200

    Finished infix to postfix but something is wrong in line 101

--- src/Stack.java
+++ src/Stack.java
@@ -1,27 +1,27 @@
 public interface Stack {
 	
 	// Pushes an item onto the top of this stack.
-	public default void push(Object o) {
+	default void push(Object o) {
 	}
 
 	// Removes the object at the top of this stack and returns that object as the value of this function.
-	public default void pop() throws StackUnderflowException {
+	default void pop() throws StackUnderflowException {
 	}
 
-	// Looks at the object at the top of this stack without removing it from the stack.
-	public default Object top() {
+	// Look at the object at the top of this stack without removing it from the stack.
+	default Object top() {
 		return null;
 	}
 
 	// Tests if this stack is empty.
-	public default boolean isEmpty() {
+	default boolean isEmpty() {
 		return false;
 	}
-
-	// ...
-	public default void empty() {
+    // knows what's on top without removing it
+	// empties the stack
+	default void empty() {
 	}
 	
-	// ...
-	public String toString();
+	// read what is inside the nodes of the stack and then print the info inside as a string
+	String toString();
 }
